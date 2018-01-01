3fb1111f5d6684e5bdba15b5f72ddf3b316a351f
Colin <colin1207@gmx.net>
2018-01-01 03:24:51 +0100

    added console log on command execution

--- js/Baka Bot.js
+++ js/Baka Bot.js
@@ -2,12 +2,13 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const CommandHandler = require("./CommandHandler");
 
-var client = new Discord.Client();
+global.client = new Discord.Client();
 global.config = JSON.parse(fs.readFileSync("config.json"));
 
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+	client.user.setGame("#BottoLifesMatter");
 });
 
 client.login(config.token);
