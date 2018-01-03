74ae66378364f50fa08058989177c1da7a464bd0
DasDoge <32263284+DasDoge@users.noreply.github.com>
2018-01-03 05:12:33 +0100

    cookiezi god command

--- js/Features/Image.js
+++ js/Features/Image.js
@@ -1,3 +1,7 @@
 exports.perhaps = function(msg) {
   msg.channel.send(new Discord.Attachment("media/perhaps.png"));
 }
+
+exports.god = function(msg) {
+  msg.channel.send(new Discord.Attachment("media/god.jpg"));
+}
