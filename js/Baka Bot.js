31fc67287194f82015ca79991dafdda7609ac3f8
Colin <colin1207@gmx.net>
2018-01-08 07:27:12 +0100

    automatic delayed reconnect on disconnect, not tested

--- js/Baka Bot.js
+++ js/Baka Bot.js
@@ -5,13 +5,24 @@ const CommandHandler = require("./CommandHandler");
 global.client = new Discord.Client();
 global.config = JSON.parse(fs.readFileSync("config.json"));
 
+var timeOut = 0;
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+	timeOut = 0;
 	client.user.setGame("#BottoLifesMatter");
 });
 
-client.login(config.token);
+client.on("disconnect", e => {
+	timeOut = timeOut + 1;
+	setTimeOut(login, Math.pow(2, timeOut));
+});
+
+var login = function(){
+	client.login(config.token);
+}
+
+login();
 
 client.on("message", msg => {
   if (msg.author.bot) return;
