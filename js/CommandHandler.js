0f66a3afca1f5b3bc4e794c9e86163b411b13bdb
DasDoge <32263284+DasDoge@users.noreply.github.com>
2018-01-16 23:13:00 +0100

    everything is made smarter now and is better than ever.

--- js/CommandHandler.js
+++ js/CommandHandler.js
@@ -1,61 +1,23 @@
-const Image = require("./Features/Image");
-const Standard = require("./Features/Standard");
-const Text = require("./Features/Text");
-const Voice = require("./Features/Voice");
 
-exports.check = function (msg) {
-  let prefix = config.prefix;
-  if (msg.content === config.tableflip || msg.content === config.unflip)
-    Standard.table(msg);
-  if (msg.content.substring(0,prefix.length) !== prefix) return;
+exports.check = msg => {
+
+  if (msg.mentions.everyone)
+    return msg.channel.send("<:mention:400370585584271362>");
+  if (msg.author.bot) return;
+  console.log("a");
+  let prefix = Baka.config.prefix;
+
+  if (!msg.content.startsWith(prefix)) return;
   let cmd = msg.content.substring(prefix.length).split(" ")[0].toLowerCase();
-  let arg = msg.content.split(cmd)[1];
-  let triggered = true;
-  switch (cmd) {
-    //Text commands
-    case "ping":
-      Text.ping(msg);
-      break;
-    case "say":
-      Text.say(msg, arg);
-      break;
-    case "git":
-      Text.git(msg);
-      break;
-    case "owo":
-      Text.owo(msg);
-      break;
-    case "help":
-      Text.help(msg);
-      break;
-    //Image commands
-    case "perhaps":
-      Image.perhaps(msg);
-      break;
-    case "god":
-      Image.god(msg);
-      break;
-    case "maniagod":
-      Image.maniagod(msg);
-      break;
-      //Other Commands
-    case "e621":
-      Standard.e621(msg);
-      msg.channel.stopTyping();
-      break;
-    case "banmyself":
-      Standard.banmyself(msg);
-      break;
-    break;
-    default:
-      triggered = false;
-  }
-  if (triggered) {
+  let arg = msg.content.substring(prefix.length).split(" ").splice(1);
+
+  if(cmd in Baka.commands) {
+    console.log("b");
+    Baka.commands[cmd].func(msg, arg);
     if (msg.channel.type === "text")
       console.log(`${msg.guild.name}#${msg.channel.name}@${msg.author.username}> ${msg.content}`);
     else if (msg.channel.type === "dm")
       console.log(`DM@${msg.author.username}> ${msg.content}`);
-    else if (msg.channel.type === "group")
-      console.log(`GroupDM@${msg.author.username}> ${msg.content}`);
   }
+
 }
--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 echo.
-node "js\Baka Bot"
+node "./js/Baka Bot" -i
 pause
