87ab64af50738cb6554cdbb36c5d17bb5fa6a0bb
DasDoge <32263284+DasDoge@users.noreply.github.com>
2018-01-23 23:32:12 +0100

    spell command

---
+++ js/Commands/spell.js
@@ -0,0 +1,26 @@
+module.exports = {
+  description_engrish: "get the correct spelling of a word",
+  description_nazi: "korrekte Schreibweise eines Wortes",
+  usage: "spell <syllable 1> [syllable 2] [...]",
+  func: (msg, arg) => {
+    if (arg.length == 0)
+      return msg.channel.send(":x: you have to specify a word")
+    for (let n = 0; n < arg.length; n++) {
+      arg[n] = shuffle(arg[n]);
+    }
+    return msg.channel.send(arg.join(""));
+  }
+}
+
+function shuffle (str) {
+    let a = str.split("");
+    let n = a.length;
+
+    for(let i = n - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    return a.join("");
+}
