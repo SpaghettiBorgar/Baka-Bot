55d40424c36933ac33142f9437995b1c6a2f06bd
DasDoge <32263284+DasDoge@users.noreply.github.com>
2018-01-28 18:57:52 +0100

    for safety

--- js/Commands/god.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  description_engrish: "#1",
-  description_german: "#1",
-  usage: "god",
-  func: (msg, arg) => {
-      msg.channel.send(new Discord.Attachment("media/god.jpg"));
-  }
-}
