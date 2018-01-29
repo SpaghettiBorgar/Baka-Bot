ae25af2885c65b62ede38f3514ad39b5d02479a2
DasDoge <32263284+DasDoge@users.noreply.github.com>
2018-01-29 22:18:20 +0100

    yowai command

---
+++ js/Commands/yowai.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description_engrish: "weak",
+  description_nazi: "schwach",
+  usage: "yowai",
+  func: (msg, arg) => {
+      msg.channel.send("https://streamable.com/wnnao");
+  }
+}
