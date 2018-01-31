1aed6576d01b8f6819ec81d38b07b85764e40acb
DasDoge <32263284+DasDoge@users.noreply.github.com>
2018-01-31 23:04:12 +0100

    Why? Why not?

---
+++ js/Commands/deanherbertpeppy.js
@@ -0,0 +1,14 @@
+var dean = require("./dean");
+var herbert = require("./herbert");
+var peppy = require("./peppy");
+
+module.exports = {
+  description_engrish: "Dean Herbert (peppy)",
+  description_german: "Dean Herbert (peppy)",
+  usage: "deanherbertpeppy",
+  func: (msg, arg) => {
+    dean.func(msg, arg);
+    herbert.func(msg, arg);
+    peppy.func(msg, arg);
+  }
+}
