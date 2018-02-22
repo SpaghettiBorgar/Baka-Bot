01f6cd33d283d36cb2bf588e66b1759e7908eb6d
DasDoge <32263284+DasDoge@users.noreply.github.com>
2018-02-22 22:28:42 +0100

    fuck

--- js/Commands/longneko.js
+++ js/Commands/longneko.js
@@ -6,7 +6,7 @@ module.exports = {
   func: (msg, arg) => {
     let len = arg[0] ? Math.round(parseFloat(arg[0])) : 1;
     if (len == null || isNaN(len) || len > 100)
-      msg.channel.send(":x:invalid length");
+      return msg.channel.send(":x:invalid length");
     let text = "\n";
     text += n1 + "\n";
     for (let i = 0; i < len; i++) {
